2a846c90fa9691c210a307d3e72d9113beca8413
Karnapuumamaheswari <karnapuumamaheswari022005@gmail.com>
2025-12-16 21:42:03 +0530

    Added second react app

---
+++ second-react-app/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        fontSize: "14px",
+        color: "gray",
+        padding: "10px"
+      }}
+    >
+      <h2>This is Footer</h2>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ second-react-app/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar() {
+  return (
+    <div
+      style={{
+        backgroundColor: "#333",
+        color: "white",
+        padding: "15px",
+        textAlign: "center"
+      }}
+    >
+      <h2>This is Navbar</h2>
+    </div>
+  );
+}
+
+export default Navbar;
